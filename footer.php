8dd60b935887e2ff42fb5bac56b37a65fc23f535
KAYKY DE SOUZA CUSTODIO - SENAI <kayky.lucas.quark@gmail.com>
2024-08-01 10:58:17 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer>
+	<div	class="container">
+		<img	src="img/logo-rodape.png" alt="Logo	Mirror	Fashion">
+		<ul	class="social">
+			<li><a	href="http://facebook.com/mirrorfashion">Facebook</a></li>
+			<li><a	href="http://twitter.com/mirrorfashion">Twitter</a></li>
+			<li><a	href="http://plus.google.com/mirrorfashion">Google+</a></li>
+		</ul>
+	</div>
+</footer>
+</body>
+</html>
